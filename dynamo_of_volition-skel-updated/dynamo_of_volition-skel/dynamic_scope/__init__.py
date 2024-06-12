af914e56b942fd175b33d9b426258934c99c6043
Tyler Beck <tylerprojects164@gmail.com>
2024-06-12 19:50:19 -0400

    test

--- dynamo_of_volition-skel-updated/dynamo_of_volition-skel/dynamic_scope/__init__.py
+++ dynamo_of_volition-skel-updated/dynamo_of_volition-skel/dynamic_scope/__init__.py
@@ -8,6 +8,38 @@ class DynamicScope(abc.Mapping):
     def __init__(self):
         self.env: Dict[str, Optional[Any]] = {}
 
+    def __getitem__(self, key: str) -> Any:
+        if key not in self.env:
+            raise NameError(f"Name '{key}' is not defined.")
+        return self.env[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.env[key] = value
+
+    def __iter__(self) -> Iterator[str]:
+        return iter(self.env)
+
+    def __len__(self) -> int:
+        return len(self.env)
+
 
 def get_dynamic_re() -> DynamicScope:
-    return None
+    dynamic_scope = DynamicScope()
+    frames = inspect.stack()[1:]  # Exclude the current frame
+
+    for frame_info in reversed(frames):
+        frame = frame_info.frame
+        local_vars = frame.f_locals
+        free_vars = frame.f_code.co_freevars
+
+        for var_name, value in local_vars.items():
+            if var_name not in free_vars and var_name not in dynamic_scope:
+                dynamic_scope[var_name] = value
+
+        # Extra credit: Check for referenced but undefined local variables
+        all_vars = set(frame.f_code.co_varnames + frame.f_code.co_cellvars)
+        undefined_vars = all_vars - set(local_vars.keys())
+        if undefined_vars:
+            raise UnboundLocalError(f"Local variable(s) {', '.join(undefined_vars)} referenced before assignment.")
+
+    return dynamic_scope
