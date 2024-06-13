3f993317df85f87675a38be6e0b255d9e5671888
Tyler Beck <tylerprojects164@gmail.com>
2024-06-12 20:56:31 -0400

    updated

--- dynamo_of_volition-skel-updated/dynamo_of_volition-skel/dynamic_scope/__init__.py
+++ dynamo_of_volition-skel-updated/dynamo_of_volition-skel/dynamic_scope/__init__.py
@@ -9,9 +9,7 @@ class DynamicScope(abc.Mapping):
         self.env: Dict[str, Optional[Any]] = {}
 
     def __getitem__(self, key: str) -> Any:
-        if key not in self.env:
-            raise NameError(f"Name '{key}' is not defined.")
-        return self.env[key]
+        return self.env.get(key, False)
 
     def __setitem__(self, key: str, value: Any) -> None:
         self.env[key] = value
@@ -24,4 +22,18 @@ class DynamicScope(abc.Mapping):
 
 
 def get_dynamic_re() -> DynamicScope:
-    return None
+    dynamic_scope = DynamicScope()
+    frames = inspect.stack()[1:]  # Exclude the current frame
+
+    for frame_info in reversed(frames):
+        frame = frame_info.frame
+        local_vars = frame.f_locals
+        free_vars = frame.f_code.co_freevars
+
+        for var_name, value in local_vars.items():
+            if var_name not in free_vars and var_name not in dynamic_scope:
+                dynamic_scope[var_name] = value
+
+        
+
+    return dynamic_scope
